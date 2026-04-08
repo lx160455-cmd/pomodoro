cbb7b599636e3f7461c1fe8049bd860ba2ed72ea
lx160455-cmd <lx160455@gmail.com>
2026-04-08 20:18:15 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'pomodoro-v1';
+const urlsToCache = [
+  '/pomodoro.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+});
